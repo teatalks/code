0f78505a7f3ae92edf6b8116f35cb8788c6e4b1a
squad12-devops <79678072+squad12-devops@users.noreply.github.com>
2021-03-07 11:03:46 -0500

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -9,7 +9,7 @@ sudo apt-get -y update
 sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update --ignore-hold
+sudo apt -y update
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt -y install docker-ce
 sudo systemctl status docker
