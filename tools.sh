27214d9cc09362740cc5cb0bda553f6faedfa552
squad12-devops <79678072+squad12-devops@users.noreply.github.com>
2021-03-07 10:34:21 -0500

    decker

--- tools.sh
+++ tools.sh
@@ -1,5 +1,15 @@
-#Ansible installation
+# Ansible installation
 sudo apt update
 sudo apt-add repository ppa:ansible/ansible -y
 sudo apt-get install ansible -y
 ansible --version
+
+# Docker installation and setup
+sudo apt-get update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
+sudo apt update -y
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
