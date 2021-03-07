97b4f72bd622b619c753a376b68439a9f86b7880
squad12-devops <79678072+squad12-devops@users.noreply.github.com>
2021-03-07 09:15:38 -0500

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,5 +1,10 @@
-#Ansible installation and setup
+#Ansible installation
+
 sudo apt update
 sudo apt-add repository ppa:ansible/ansible -y
 sudo apt-get install ansible -y
 ansible --version
+
+# Ansible environment setup
+sudo cp /etc/ansible/hosts /etc/ansible/hosts_"$(date +"%Y_%m_%d_%I_%M_%p")"
+
