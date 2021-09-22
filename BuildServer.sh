efbee01fd21f13458e2e8ba74c9f6f63ad979655
teatalks <82669305+teatalks@users.noreply.github.com>
2021-09-22 07:26:38 -0400

    Create BuildServer.sh

---
+++ BuildServer.sh
@@ -0,0 +1,7 @@
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt-get -y update && sudo apt-get -y upgrade
+sudo apt -y install openjdk-8-jdk
+sudo apt-get -y install jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
